41701a7f5550ccd591d5f34cf2ee46c9471f8fd3
AnkushNakaskar <ankush.nakaskar@gmail.com>
2017-05-12 09:52:14 +0530

    first commit

---
+++ app/js/controller/loginController.js
@@ -0,0 +1,5 @@
+myApp.controller('loginController', ['$scope', function($scope) {
+  $scope.double = function(value) { return value * 2; };
+    $scope.name="Ankush";
+    console.log("My first angularJs project...!!!");
+}]);
